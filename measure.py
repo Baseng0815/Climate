2cee0d60baaddd27847b4b5ff065b34f4c71837d
Baseng0815 <1shedex2@gmail.com>
2022-07-24 18:22:46 +0100

    remove sensor.py, modify measure.py to use postgres

--- measure.py
+++ measure.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 import adafruit_dht
-import pymongo
+import psycopg2
 from datetime import datetime
 
 data_pin = 4
 sensor = adafruit_dht.DHT22(data_pin)
 
-db_client = pymongo.MongoClient('mongodb://localhost:27017/')
-db_collection = db_client.hord.climate
+db_connection = psycopg2.connect(user='pi', host='127.0.0.1', port='5432', database='pi')
+db_cursor = db_connection.cursor()
 
 temperature = sensor.temperature
 humidity = sensor.humidity
 if humidity is not None and temperature is not None:
     print(f"Temperature={temperature} humidity={humidity}")
-    db_collection.insert_one({'when': datetime.now().isoformat(),
-                              'temperature': temperature, 'humidity': humidity})
+    db_cursor.execute(f'INSERT INTO climate VALUES (\'{datetime.now().isoformat()}\', {temperature}, {humidity})')
+    db_connection.commit()
